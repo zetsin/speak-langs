b0d8456e2738af15dc30acbb6b3c873491c35d69
Zetsin <zetsin@gmail.com>
2018-05-26 20:20:56 +0800

    init

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,9 +11,7 @@ router.get('/google', passport.authenticate('google', {
 
 router.get('/google/oauth2callback', (req, res, next) => {
   if(url.parse(req.get('referer')).host === process.env.dev_client_host && req.get('host') !== process.env.dev_client_server_host) {
-    console.log(process.env.dev_client_server_host, req.originalUrl)
-    console.log(url.resolve(process.env.dev_client_server_host, req.originalUrl))
-    res.redirect(url.resolve(process.env.dev_client_server_host, req.originalUrl))
+    res.redirect(process.env.dev_client_server_host + req.originalUrl)
   }
   else {
     next()
